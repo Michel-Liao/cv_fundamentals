105331411886f5d7e97643fe90696dc7ecb7f498
Michel-Liao <michel.liao22@gmail.com>
2023-11-27 16:21:20 -0500

    Finish gray_scale.py

--- assignment_2/gray_scale.py
+++ assignment_2/gray_scale.py
@@ -13,12 +13,18 @@ height, width, channels = pic.shape
 
 fig, ax = plt.subplots(1, 1, figsize=(12, 8))
 ax.imshow(pic)
-plt.show()
+# plt.show()
 
 # changing to grayscale
 for i in range(height):
     for j in range(width):
         pic[i,j] = np.sum(pic[i,j]) / 3
 
-ax.imshow(pic)
-plt.show()
+print(pic)
+
+pic = np.delete(pic, pic[:, :, :3], axis=1)
+# not exactly sure why axis=1 here? otherwise the image gets flattened
+
+# save grayscale image
+img = Image.fromarray(pic)
+img.save('englishcreams_grayscale.png')
