274d2cba7e9e516618d46aacadf9665b6ffa0714
Michel-Liao <michel.liao22@gmail.com>
2023-12-09 20:46:17 -0500

    Finish reshaping.py

--- assignment_2/reshaping.py
+++ assignment_2/reshaping.py
@@ -15,15 +15,5 @@ print(f'before transpose {pic[:2]} \n shape is {pic.shape} \n \n')
 
 pic_T1 = np.transpose(pic, (2, 0, 1))
 print(f'after transpose {pic_T1[:2]} \n shape is {pic_T1.shape}')
-plt.imshow(pic_T1[2]) # how can i tell what channel this is if i didn't know rgb order?
+plt.imshow(pic_T1[1])
 plt.show()
-
-# desired shape: height, width, channels
-
-
-# pic2 = pic.flatten()
-# print(pic[:6])
-# print(pic2[:6])
-# breakpoint()
-# look intro np.transpose or torch.permute
-
