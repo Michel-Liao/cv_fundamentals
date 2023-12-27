8159f79ea4dd0453882feeb31a05f3fc39543011
Michel-Liao <michel.liao22@gmail.com>
2023-12-26 23:45:02 -0700

    Get rid of breakpoint

--- assignment_2/gray_scale.py
+++ assignment_2/gray_scale.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 # import cv2 # doesn't work
 from PIL import Image
 
 im = Image.open("englishcreams.png")
-im = im.convert('RGB')
+im = im.convert("RGB")
 pic = np.array(im)
 # height, width, channels (rgba)
 height, width, channels = pic.shape
@@ -18,13 +19,12 @@ ax.imshow(pic)
 # changing to grayscale
 for i in range(height):
     for j in range(width):
-        pic[i,j] = np.sum(pic[i,j]) / 3
-breakpoint()
-pic = np.delete(pic, [1,2], axis=1)
-# delets columns 1 and 2
+        pic[i, j] = np.sum(pic[i, j]) / 3
+pic = np.delete(pic, [1, 2], axis=1)
+# deletes columns 1 and 2
 print(pic)
 # not sure why each pixel still has 3 values though
 
 # save grayscale image
 img = Image.fromarray(pic)
-img.save('englishcreams_grayscale.png')
+img.save("englishcreams_grayscale.png")
