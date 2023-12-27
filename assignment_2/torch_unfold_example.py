449dc7d30f965d4aa6c8a8bc20e263f564778f55
Michel-Liao <michel.liao22@gmail.com>
2023-12-27 13:18:19 -0700

    Comment code for clarity

--- assignment_2/torch_unfold_example.py
+++ assignment_2/torch_unfold_example.py
@@ -1,4 +1,5 @@
 # Example written by Erich Liang
+# Comments written by Michel Liao
 
 import torch
 import numpy as np
@@ -11,14 +12,16 @@ orig = torch.Tensor(orig)
 print(type(orig))
 
 unfold_op = torch.nn.Unfold(kernel_size=(3, 3), dilation=1, padding=0, stride=1)
+# Stride of 1 is the basic step. Move your kernel over by 1 pixel at a time.
 result = unfold_op(orig)
 
-print('orig:')
+print("orig:")
 print(orig)
 print(orig.shape)
 
-print('result:')
+print("result:")
 print(result.shape)
+# Note result[0, :, i] gives the ith kernel of orig
 print(result[0, :, 0])
 print(result[0, :, 1])
 print(result[0, :, 2])
