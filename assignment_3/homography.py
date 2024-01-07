e042e3383ec5931720fa008cd2cc2bf36c19d7ee
Michel-Liao <michel.liao22@gmail.com>
2024-01-06 17:49:23 -0700

    Code drawAxes for x-axis

--- assignment_3/homography.py
+++ assignment_3/homography.py
@@ -22,10 +22,26 @@ def evenDims(image):
     return image
 
 
+def drawAxes(image, size=6):
+    if size % 2 != 0:
+        raise ValueError("Size must be even.")
+
+    height, width, channels = image.shape
+    if height % 2 != 0 or width % 2 != 0:
+        raise ValueError("Image must have even dimensions.")
+
+    # Draw x-axis
+    xAx = np.zeros((size, width, channels), dtype=image.dtype)
+    image[int((height / 2 - size / 2)) : int((height / 2 + size / 2)), :] = xAx
+
+    return image
+
+
 # Read and show image
 img = cv.imread("central_park.jpg")
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 img = evenDims(img)
+img = drawAxes(img)
 
 fig, ax = plt.subplots(1, 1, figsize=(12, 8))
 ax.imshow(img)
